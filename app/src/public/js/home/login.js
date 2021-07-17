39ed461de99d584706907a71f743d1e6e09ae78a
sohyun <shparkso162@gmail.com>
2021-07-18 07:44:32 +0900

    save HTML object using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"),
+    pwd = document.querySelector("#pwd"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pwd : pwd.value,
+    };
+}
