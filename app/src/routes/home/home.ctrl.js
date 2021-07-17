0d449ff85ea7d5e5cb96e942ca4b7beedeacaf27
sohyun <shparkso162@gmail.com>
2021-07-18 08:33:02 +0900

    create login action at backend

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,39 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const users = {
+    id: ["sohyun", "mimi", "gogo"],
+    pwd: ["1234", "1234", "1234"],
 };
 
-const login = (req, res) =>{
-    res.render("home/login");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login : (req, res) => {
+        res.render("home/login");
+    },
+}
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            pwd = req.body.pwd;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.pwd[idx] === pwd){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "fail to login",
+        });
+    },
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
